dfacc242f2a9e419d67b034375f6c35ff4d1ccf3
murphy <987127314@qq.com>
2020-12-23 09:02:08 +0800

    deploy

---
+++ precache-manifest.320ea3aae27a72a4c996289c2b70ab24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aeafc1171fa02088445",
+    "url": "/murney-website/css/app.d0762908.css"
+  },
+  {
+    "revision": "a30807e72ab49283aa0bd75e9353ab5d",
+    "url": "/murney-website/index.html"
+  },
+  {
+    "revision": "1aeafc1171fa02088445",
+    "url": "/murney-website/js/app.0997823d.js"
+  },
+  {
+    "revision": "205ba48c7ee7a13251cc",
+    "url": "/murney-website/js/chunk-vendors.4a444add.js"
+  },
+  {
+    "revision": "33390c028309695f846cc2ea268833da",
+    "url": "/murney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/murney-website/robots.txt"
+  }
+]);
